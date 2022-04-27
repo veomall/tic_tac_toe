0771c9b664432cb0d8b91420ba764e6a376f024e
Georgiy <86735084+veomall@users.noreply.github.com>
2022-04-27 23:37:30 +0300

    py

--- criss_cross.py
+++ criss_cross.py
@@ -1,10 +1,12 @@
 from tkinter import *
 import random
 root = Tk()
-root.title('Criss-cross')
+root.title('Tic-tac-toe')
 game_run = True
 field = []
 cross_count = 0
+
+
 def new_game():
     for row in range(3):
         for col in range(3):
@@ -14,6 +16,7 @@ def new_game():
     game_run = True
     global cross_count
     cross_count = 0
+
 
 def click(row, col):
     if game_run and field[row][col]['text'] == ' ':
@@ -25,6 +28,7 @@ def click(row, col):
             computer_move()
             check_win('O')
 
+
 def check_win(smb):
     for n in range(3):
         check_line(field[n][0], field[n][1], field[n][2], smb)
@@ -32,11 +36,13 @@ def check_win(smb):
     check_line(field[0][0], field[1][1], field[2][2], smb)
     check_line(field[2][0], field[1][1], field[0][2], smb)
 
+
 def check_line(a1,a2,a3,smb):
     if a1['text'] == smb and a2['text'] == smb and a3['text'] == smb:
         a1['background'] = a2['background'] = a3['background'] = 'pink'
         global game_run
         game_run = False
+
 
 def can_win(a1,a2,a3,smb):
     res = False
@@ -50,6 +56,7 @@ def can_win(a1,a2,a3,smb):
         a1['text'] = 'O'
         res = True
     return res
+
 
 def computer_move():
     for n in range(3):
@@ -70,44 +77,39 @@ def computer_move():
         return
     if can_win(field[2][0], field[1][1], field[0][2], 'X'):
         return
-    while True:
-        if cross_count == 1 and field[1][1]['text'] == ' ':
-            field[1][1]['text'] = 'O'
-            break
-        elif cross_count == 1 and field[1][1]['text'] == 'X':
+
+    if cross_count == 1 and field[1][1]['text'] == ' ':
+        field[1][1]['text'] = 'O'
+    elif cross_count == 1 and field[1][1]['text'] == 'X':
+        field[0][0]['text'] = 'O'
+    elif cross_count == 2 and field[1][1]['text'] == 'X' and field[2][2]['text'] == 'X':
+        field[2][0]['text'] = 'O'
+    elif cross_count == 2 and field[0][1]['text'] == 'X':
+        if field[1][0]['text'] == 'X':
             field[0][0]['text'] = 'O'
-            break
-        elif cross_count == 2 and field[1][1]['text'] == 'X' and field[2][2]['text'] == 'X':
+        elif field[1][2]['text'] == 'X' or field[2][1]['text'] == 'X':
+            field[0][2]['text'] = 'O'
+    elif cross_count == 2 and field[2][1]['text'] == 'X':
+        if field[1][0]['text'] == 'X':
             field[2][0]['text'] = 'O'
-            break
-        elif cross_count == 2 and field[0][1]['text'] == 'X':
-            if field[1][0]['text'] == 'X':
-                field[0][0]['text'] = 'O'
-                break
-            elif field[1][2]['text'] == 'X':
-                field[0][2]['text'] = 'O'
-                break
-        elif cross_count == 2 and field[2][1]['text'] == 'X': 
-            if field[1][0]['text'] == 'X':
-                field[2][0]['text'] = 'O'
-                break
-            elif field[1][2]['text'] == 'X':
-                field[2][2]['text'] = 'O'
-                break
-        elif cross_count == 2 and field[0][1]['text'] == 'X' and field[2][1]['text'] == 'X':
-            field[0][0]['text'] == 'O'
-        else:
-            row = random.randint(0, 2)
-            col = random.randint(0, 2)
-            if field[row][col]['text'] == ' ':
-                field[row][col]['text'] = 'O'
-                break
+        elif field[1][2]['text'] == 'X':
+            field[2][2]['text'] = 'O'
+    elif cross_count == 2 and field[0][1]['text'] == 'X' and field[2][1]['text'] == 'X':
+        field[0][0]['text'] = 'O'
+    elif field[1][0]['text'] == 'X' and cross_count == 2 and field[1][2]['text'] == 'X':
+        field[0][2]['text'] = 'O'
+    else:
+        row = random.randint(0, 2)
+        col = random.randint(0, 2)
+        if field[row][col]['text'] == ' ':
+            field[row][col]['text'] = 'O'
+
 
 for row in range(3):
     line = []
     for col in range(3):
-        button = Button(root, text=' ', width=8, height=4, 
-                        font=('Verdana', 10, 'bold'),
+        button = Button(root, text=' ', width=8, height=4,
+                        font=('Verdana', 20, 'bold'),
                         background='lavender',
                         command=lambda row=row, col=col: click(row,col))
         button.grid(row=row, column=col, sticky='ns')
